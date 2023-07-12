31671479cc93cb69ad4617ad94e799d67a18a00d
unknown <masoodg1998@gmail.com>
2023-07-13 01:05:07 +0500

    latest build

--- src/index.js
+++ src/index.js
@@ -3,28 +3,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ColorContextProvider } from './Store';
-import { hydrate, render } from "react-dom";
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 
 
 
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate( <ColorContextProvider><App /></ColorContextProvider>, rootElement);
-} else {
-  render( <ColorContextProvider><App /></ColorContextProvider>, rootElement);
-}
 
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <ColorContextProvider>
-//     <App />
-//     </ColorContextProvider>
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ColorContextProvider>
+    <App />
+    </ColorContextProvider>
+  </React.StrictMode>
+);
 
 
 
